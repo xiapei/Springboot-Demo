694aff8bf7e359074c33ec318aa38ed869b15468
rexue_xia <xiapp>
2019-01-03 15:10:59 +0800

    *)jdbcTemplate查询数据

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,19 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication extends SpringBootServletInitializer implements CommandLineRunner {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -16,5 +23,13 @@ public class DemoApplication extends SpringBootServletInitializer {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... strings) throws Exception {
+		String sql = "select t.* from user_userinfo t where rownum < 10";
+		List<Map<String,Object>> list = jdbcTemplate.queryForList(sql);
+		for (Map<String,Object> map : list){
+			System.out.println(map.get("userid"));
+		}
+	}
 }
 
